6ae26b8ed98c3b5de62434be617c698ed4bfb454
Steve Reid <steve@appby.design>
2017-11-03 19:39:29 +0000

    Set up camel-case to snake-case name convetions in Postgres model

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
-models.sequelize.sync({}).then(() => {
+models.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log('------------------------------------');
     console.log(`GraphQL server running on port: ${PORT}`);
